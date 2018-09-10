afa9653243a1c60df1b8575571878b14e4959765
Hrvoje Bogner <hbogner@gmail.com>
2018-09-10 10:12:54 +0200

    Add gcc to setup
    Needed for osmfilter and osmconvert cimplie

--- setup-osm-replex.sh
+++ setup-osm-replex.sh
@@ -5,7 +5,7 @@
 #sudo apt-get install zip unzip openjdk-7-jre
 
 ##ubuntu 18.04.1 clean install
-#sudo apt-get install zip unzip openjdk-8-jre zlib1g-dev gdal-bin
+#sudo apt-get install zip unzip openjdk-8-jre zlib1g-dev gdal-bin gcc
 
 REPLEX=/osm/osm-replex
 
